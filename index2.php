abe5f13f488cae95b323ed2d6725e904a86ef875
ChikagoDen <perepelkin-da@yandex.ru>
2021-10-13 19:50:43 +0600

    Последняя редакция

--- index2.php
+++ index2.php
@@ -1,35 +1,32 @@
 <?php
     include ('settings.php'); // или include_once
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php 
-        foreach ($title as $str_title)
-    echo $str_title." ";
-    ?></title>
+    <title>
+        <?php 
+            foreach ($title as $str_title)
+             echo $str_title." ";
+        ?>
+    </title>
 </head>
 <body>
-
     <h1> 
         <?php 
             foreach ($h1 as $str_h1)
             echo $str_h1." "; 
         ?>
     </h1>
-  
     <ul class="menu">
         <li><a href="catalog.html">Каталог</a></li>
         <li><a href="contact.html">Контакты</a></li>
         <!-- <li></li> -->
     </ul>
     <hr>
-   
     <h1>Магазин <b>&laquo;ВСЕЛЕНАЯ TV&raquo;</b> </h1>
-    
     <p>Добро пожаловать в&nbsp;наш замечательный магазин &laquo;ВСЕЛЕНАЯ&nbsp;TV&raquo; с 
         самыми низкими ценами и&nbsp;самым высоким качеством.
     </p>
--- settings.php
+++ settings.php
@@ -2,4 +2,4 @@
         $h1=array("Главная страница"); 
         $title=array("Главная страница");
         $time=date("Y");
-   include "site.html";
+   
